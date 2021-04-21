64b458cc7c97721e4b7dc6f0c0c6a7fe8cf53d5e
BirukouAliaksei <birukou.aliaksei@gmail.com>
2021-04-21 11:31:15 +0300

    Feature/crypto (#8)
    * added encrytor decryptor v.1
    
    * added gcm encode/decode functions
    
    * added constructor
    
    * added positive tests and fixed nil checking
    
    * fixed errors handler and removed constructor from test class
    
    * changed tests to table tests, added to crypto constructor hash converter
    
    * fixed tests data
    
    * fixed code format
    
    * fixed test block format
    
    * fixed "Using the variable on range scope"
    
    * fixed variable scope ver.2
    
    * added crypto package description
    
    * fixed comments and visibility of the fields
    
    * change cryptograper to cryptographer

---
+++ pkg/crypto/crypto.go
@@ -0,0 +1,75 @@
+/*
+	Package crypto provides functions to encode and decode data
+	Using aes crypto
+*/
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"io"
+)
+
+type cryptographer struct {
+	key        []byte
+	randomFlag bool
+}
+
+func NewCryptographer(key []byte) *cryptographer {
+	h := sha256.New()
+	key32 := make([]byte, 32)
+	copy(key32, h.Sum(key))
+	return &cryptographer{
+		key:        key32,
+		randomFlag: true,
+	}
+}
+
+func (c *cryptographer) Encode(value []byte) ([]byte, error) {
+	// Create a new Cipher Block from the key
+	block, err := aes.NewCipher(c.key)
+	if err != nil {
+		return nil, fmt.Errorf("cryptographer, encode method: invalid key: %w", err)
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("cryptographer, encode method: invalid size: %w", err)
+	}
+	// Create a nonce. Nonce should be from GCM
+	nonce := make([]byte, aesGCM.NonceSize())
+	if c.randomFlag {
+		if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+			return nil, fmt.Errorf("cryptographer, encode method: unexpected data: %w", err)
+		}
+	}
+	// Encrypt the data using aesGCM.Seal
+	// Since we don't want to save the nonce somewhere else in this case,
+	// we add it as a prefix to the encrypted data. The first nonce argument in Seal is the prefix.
+	ciphertext := aesGCM.Seal(nonce, nonce, value, nil)
+	return ciphertext, nil
+}
+
+func (c *cryptographer) Decode(encodedValue []byte) ([]byte, error) {
+	block, err := aes.NewCipher(c.key)
+	if err != nil {
+		return nil, fmt.Errorf("cryptographer, decode method: invalid key: %w", err)
+	}
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("cryptographer, decode method: invalid size: %w", err)
+	}
+	// Get the nonce size
+	nonceSize := aesGCM.NonceSize()
+	// Extract the nonce from the encrypted data
+	nonce, ciphertext := encodedValue[:nonceSize], encodedValue[nonceSize:]
+	// Decrypt the data
+	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cryptographer, decode method: decryption error: %w", err)
+	}
+	return plaintext, nil
+}
---
+++ pkg/crypto/crypto_test.go
@@ -0,0 +1,62 @@
+package crypto
+
+import (
+	"bytes"
+	"testing"
+)
+
+var tests = []struct {
+	name  string
+	key   []byte
+	value []byte
+	want  []byte
+}{
+	{"encode/decode value 1", []byte("I am the key"), []byte("All i need is love"),
+		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 103, 219, 30, 14, 204, 218, 207, 85, 148, 66,
+			160, 22, 65, 82, 133, 234, 239, 51, 104, 63, 206, 168, 142, 30, 10, 255, 243, 84, 85, 36, 201, 78, 183, 51}},
+	{"encode/decode value 2", []byte("I am another key"), []byte("All i need is love love love"),
+		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 170, 62, 141, 236, 82, 152, 165, 131, 27, 42, 227, 232, 125, 148,
+			125, 23, 77, 242, 99, 196, 24, 2, 221, 82, 234, 5, 16, 73, 28, 153, 134, 58,
+			197, 134, 198, 43, 27, 28, 162, 145, 82, 157, 6, 121}},
+	{"encode/decode with key match more than 32", []byte("werwewtwtwrtrtert55tttttttttttttggggggggggggrt56456hfghfhj$34g"), []byte("All i need is love"),
+		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 239, 244, 87, 184, 57, 133, 139, 86, 139, 202, 207, 137, 43, 33, 61, 69,
+			145, 190, 115, 24, 108, 30, 166, 64, 159, 130, 62, 228, 164, 223, 128, 107, 149, 252}},
+	{"empty key and value", []byte(""), []byte(""),
+		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 213, 30, 214, 8, 30, 219, 152, 115, 144, 128, 251, 224, 158, 196, 118, 251}},
+}
+
+func TestCryptographer_Encode(t *testing.T) {
+	for i, tt := range tests {
+		t.Logf("\tTest: %d\tfor key %q and value %q", i+1, tt.key, tt.value)
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			encode := NewCryptographer(tt.key)
+			encode.randomFlag = false
+			got, err := encode.Encode(tt.value)
+			if err != nil {
+				return
+			}
+			if !bytes.Equal(got, tt.want) {
+				t.Errorf(string(got), tt.want)
+			}
+		})
+	}
+}
+
+func TestCryptographer_Decode(t *testing.T) {
+	for i, tt := range tests {
+		t.Logf("\tTest: %d\tfor key %q and value %q", i+1, tt.key, tt.value)
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			decode := NewCryptographer(tt.key)
+			decode.randomFlag = false
+			got, err := decode.Decode(tt.want)
+			if err != nil {
+				return
+			}
+			if !bytes.Equal(got, tt.value) {
+				t.Errorf(string(got), tt.want)
+			}
+		})
+	}
+}

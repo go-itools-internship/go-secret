104f61805ec222f23cbcf274d39f481739dde127
ViktorRyneiski <gizzzer@tut.by>
2021-04-22 03:22:46 +0300

    fix comment

--- pkg/io/storage/storage.go
+++ pkg/io/storage/storage.go
@@ -18,8 +18,8 @@ type fileVault struct {
 func NewFileVault(path string) (*fileVault, error) {
 	var storage = make(map[string][]byte)
 
-	//open the file at the specified path, create it if the file is not found
-	file, err := os.OpenFile(path, os.O_CREATE, 0600)
+	// open the file at the specified path, create it if the file is not found
+	file, err := os.OpenFile(path, os.O_CREATE, 0777)
 	if err != nil {
 		return nil, fmt.Errorf("unable to open or create file: %w", err)
 	}
@@ -30,17 +30,17 @@ func NewFileVault(path string) (*fileVault, error) {
 
 func (f *fileVault) SaveData(key, encodedValue []byte) error {
 
-	//open the file for writing only
+	// open the file for writing only
 	file, err := os.OpenFile(f.path, os.O_WRONLY, 0777)
 	if err != nil {
 		return fmt.Errorf("unable to open file: %w", err)
 	}
 	defer file.Close()
 
-	//set coming encodedValue to our storage
+	// set coming encodedValue to our storage
 	f.storage[string(key)] = encodedValue
 
-	//encode the data in storage and put it to file in json format
+	// encode the data in storage and put it to file in json format
 	err = json.NewEncoder(file).Encode(f.storage)
 	if err != nil {
 		return fmt.Errorf("unable to write data from map to file: %w", err)
@@ -55,7 +55,7 @@ func (f *fileVault) ReadData(key []byte) ([]byte, error) {
 	}
 	defer file.Close()
 
-	//decode data from json format from file and put it in storage
+	// decode data from json format from file and put it in storage
 	err = json.NewDecoder(file).Decode(&f.storage)
 	if err != nil {
 		return nil, fmt.Errorf("unable to write data from file to map: %w", err)

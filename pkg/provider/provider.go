411c912a40afe53ac3527ded4a9a7a832b7aa079
Birukou Aliaksei <Birukou.aliaksei@gmail.com>
2021-04-23 18:18:09 +0300

    added errors tests and fix error messages in provider file

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -18,11 +18,11 @@ func NewProvider(cryptographer secret.Cryptographer, dataSaver secret.DataSaver)
 func (p *provider) SetData(key, value []byte) error {
 	encodedValue, err := p.cryptographer.Encode(value)
 	if err != nil {
-		return fmt.Errorf("provider, SetData method: error: %w", err)
+		return fmt.Errorf("provider, SetData method: encode error: %w", err)
 	}
 	saveError := p.dataSaver.SaveData(key, encodedValue)
 	if saveError != nil {
-		return fmt.Errorf("provider, SetData method: save error: %w", err)
+		return fmt.Errorf("provider, SetData method: save error: %w", saveError)
 	}
 	return nil
 }
@@ -30,11 +30,11 @@ func (p *provider) SetData(key, value []byte) error {
 func (p *provider) GetData(key []byte) ([]byte, error) {
 	data, err := p.dataSaver.ReadData(key)
 	if err != nil {
-		return nil, fmt.Errorf("provider, GetData method: error: %w", err)
+		return nil, fmt.Errorf("provider, GetData method: read data error: %w", err)
 	}
 	decode, err := p.cryptographer.Decode(data)
 	if err != nil {
-		return nil, fmt.Errorf("provider, GetData method: error: %w", err)
+		return nil, fmt.Errorf("provider, GetData method: decode error: %w", err)
 	}
 	return decode, nil
 }

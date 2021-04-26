66c3f991a9ba8aec0b6a02b7f589b11ec8f75027
Alex Rudyak <aliaksandr.rudziak@instinctools.com>
2021-04-26 14:25:39 +0300

    fix: rename http handler structure to 'methods' structure
    * add descriptive errors for non 2XX responses
    
    * add tests

--- pkg/http/handlers.go
+++ pkg/http/handlers.go
@@ -1,10 +1,12 @@
 /*
-Package http provides handlers for different actions for REST API.
+Package http provides handlers for different methods for REST API.
 */
 package http
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,33 +14,44 @@ import (
 )
 
 const (
-	ParamActionKey = "action"
+	ParamMethodKey = "method"
 	ParamCipherKey = "cipher"
 	ParamGetterKey = "key"
 )
 
-type ActionFactoryFunc func(cipher string) (secret.Provider, func())
+// MethodFactoryFunc type specifies signature to create/fetch a provider
+// based on cipher key.
+// Returns also a tear down function that should be called provider's work is done.
+// Tear down function could be nil so need to check for nil before invoking.
+type MethodFactoryFunc func(cipher string) (secret.Provider, func())
 
-type actions struct {
-	ss map[string]ActionFactoryFunc
+type methods struct {
+	ss map[string]MethodFactoryFunc
 }
 
-func NewActions(ss map[string]ActionFactoryFunc) *actions {
-	return &actions{
+// NewMethods initializes a structure that provides HTTP handler functions
+// to organize REST API access to different type of provides based on "method" type.
+//
+// Accepts `ss` map with a set of method-provider pair.
+func NewMethods(ss map[string]MethodFactoryFunc) *methods {
+	return &methods{
 		ss: ss,
 	}
 }
 
-func (a *actions) GetByKey(w http.ResponseWriter, r *http.Request) {
+// GetByKey method fetches a value specified by getter key.
+// Uses cipher key to access encrypted data.
+// Requires to provide getter key, cipher key (as a header) and method type to access as.
+func (a *methods) GetByKey(w http.ResponseWriter, r *http.Request) {
 	getterKey := r.URL.Query().Get(ParamGetterKey)
 	if getterKey == "" {
-		w.WriteHeader(http.StatusBadRequest)
+		a.writeErrorResponse(w, http.StatusBadRequest, errors.New("cannot find getter key: empty"))
 		return
 	}
 
-	actionType := r.URL.Query().Get(ParamActionKey)
+	actionType := r.URL.Query().Get(ParamMethodKey)
 	if _, ok := a.ss[actionType]; !ok {
-		w.WriteHeader(http.StatusBadRequest)
+		a.writeErrorResponse(w, http.StatusBadRequest, fmt.Errorf("cannot find provided method type %s", actionType))
 		return
 	}
 
@@ -50,7 +63,7 @@ func (a *actions) GetByKey(w http.ResponseWriter, r *http.Request) {
 
 	result, err := p.GetData([]byte(getterKey))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		a.writeErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("cannot get data by key: %w", err))
 		return
 	}
 
@@ -59,25 +72,29 @@ func (a *actions) GetByKey(w http.ResponseWriter, r *http.Request) {
 	}
 	responseBody.Value = string(result)
 	if err := json.NewEncoder(w).Encode(responseBody); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		a.writeErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("cannot write response: %w", err))
 	}
 }
 
-/*
-{
-	"getter": "cloud-key",
-	"action": "memory",
-	"value": "123-456"
-}
-*/
-func (a *actions) SetByKey(w http.ResponseWriter, r *http.Request) {
+// SetByKey method sets a new value for specified getter key.
+// Value is encrypted using cipher key (provided in header).
+// Requires to provide getter key, cipher key (as a header) and method type to access as.
+//
+// Example of request body:
+//
+//     {
+//         "getter": "cloud-key",
+//         "method": "memory",
+//         "value": "123-456"
+//     }
+func (a *methods) SetByKey(w http.ResponseWriter, r *http.Request) {
 	var requestBody struct {
 		GetterKey  string `json:"getter"`
-		ActionType string `json:"action"`
+		MethodType string `json:"method"`
 		Value      string `json:"value"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		a.writeErrorResponse(w, http.StatusBadRequest, fmt.Errorf("cannot decode body: %w", err))
 		return
 	}
 	defer func() {
@@ -87,28 +104,34 @@ func (a *actions) SetByKey(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	if requestBody.GetterKey == "" {
-		w.WriteHeader(http.StatusBadRequest)
+		a.writeErrorResponse(w, http.StatusBadRequest, errors.New("cannot find getter key: empty"))
 		return
 	}
-	if _, ok := a.ss[requestBody.ActionType]; !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		if _, err := w.Write([]byte(`{"error": "cannot find provided action type"}`)); err != nil {
-			log.Println("cannot write response body: ", err.Error())
-		}
+	if _, ok := a.ss[requestBody.MethodType]; !ok {
+		a.writeErrorResponse(w, http.StatusBadRequest, fmt.Errorf("cannot find provided method type %s", requestBody.MethodType))
 		return
 	}
 
 	cipherKey := r.Header.Get(ParamCipherKey)
-	p, tearDownFn := a.ss[requestBody.ActionType](cipherKey)
+	p, tearDownFn := a.ss[requestBody.MethodType](cipherKey)
 	if tearDownFn != nil {
 		defer tearDownFn()
 	}
 
 	err := p.SetData([]byte(requestBody.GetterKey), []byte(requestBody.Value))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		a.writeErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("cannot set data: %w", err))
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func (a *methods) writeErrorResponse(w http.ResponseWriter, status int, response error) {
+	w.WriteHeader(status)
+	if response != nil {
+		if _, err := fmt.Fprintf(w, `{"error":"%s"}`, response.Error()); err != nil {
+			log.Println("cannot write response body: ", err.Error())
+		}
+	}
 }
--- pkg/http/handlers_test.go
+++ pkg/http/handlers_test.go
@@ -6,31 +6,39 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/go-itools-internship/go-secret/pkg/secret"
 	"github.com/stretchr/testify/require"
 )
 
-func TestHTTPAction(t *testing.T) {
+const jsonTerminator = "\n"
+
+func TestHTTPHandlers(t *testing.T) {
 	t.Run("set by key", func(t *testing.T) {
 		t.Run("success", func(t *testing.T) {
 			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
 			mockProvider.On("SetData", []byte("test-getter-1"), []byte("test-value-1")).Return(nil).Once()
 
 			expectedSipherKey := "1234-5678"
 
-			a := NewActions(map[string]ActionFactoryFunc{
-				"test-action": func(cipher string) (secret.Provider, func()) {
+			tearDownFnCounter := int64(1)
+			a := NewMethods(map[string]MethodFactoryFunc{
+				"test-method": func(cipher string) (secret.Provider, func()) {
 					require.EqualValues(t, expectedSipherKey, cipher)
-					return mockProvider, func() {}
+					return mockProvider, func() {
+						atomic.AddInt64(&tearDownFnCounter, -1)
+					}
 				},
 			})
 
 			s := httptest.NewServer(http.HandlerFunc(a.SetByKey))
 			defer s.Close()
 
-			body := bytes.NewBufferString(`{"getter":"test-getter-1","action":"test-action","value":"test-value-1"}`)
+			body := bytes.NewBufferString(`{"getter":"test-getter-1","method":"test-method","value":"test-value-1"}`)
 			req := httptest.NewRequest(http.MethodPost, s.URL, body)
 			req.Header.Set(ParamCipherKey, expectedSipherKey)
 			req.RequestURI = ""
@@ -39,21 +47,25 @@ func TestHTTPAction(t *testing.T) {
 			require.NoError(t, err)
 			require.EqualValues(t, http.StatusNoContent, resp.StatusCode)
 
-			mockProvider.AssertExpectations(t)
+			require.Eventually(t, func() bool {
+				return atomic.LoadInt64(&tearDownFnCounter) == 0
+			}, 300*time.Millisecond, 100*time.Millisecond)
 		})
 
-		t.Run("error when action is not found", func(t *testing.T) {
-			a := NewActions(map[string]ActionFactoryFunc{
-				"test-action": func(cipher string) (secret.Provider, func()) {
-					t.Error("test action should not be invoked")
-					return nil, func() {}
+		t.Run("error when method is not found", func(t *testing.T) {
+			a := NewMethods(map[string]MethodFactoryFunc{
+				"test-method": func(cipher string) (secret.Provider, func()) {
+					t.Error("test method should not be invoked")
+					return nil, func() {
+						t.Errorf("tear down should not be invoked")
+					}
 				},
 			})
 
 			s := httptest.NewServer(http.HandlerFunc(a.SetByKey))
 			defer s.Close()
 
-			body := bytes.NewBufferString(`{"getter":"test-getter-1","action":"test-action-1","value":"test-value-1"}`)
+			body := bytes.NewBufferString(`{"getter":"test-getter-1","method":"test-method-1","value":"test-value-1"}`)
 			req := httptest.NewRequest(http.MethodPost, s.URL, body)
 			req.Header.Set(ParamCipherKey, "")
 			req.RequestURI = ""
@@ -64,26 +76,27 @@ func TestHTTPAction(t *testing.T) {
 
 			respBody, err := io.ReadAll(resp.Body)
 			require.NoError(t, err)
-			require.EqualValues(t, `{"error": "cannot find provided action type"}`, respBody)
+			require.EqualValues(t, `{"error":"cannot find provided method type test-method-1"}`, respBody)
 		})
 
 		t.Run("error when provider returned error", func(t *testing.T) {
 			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
 			mockProvider.On("SetData", []byte("test-getter-1"), []byte("test-value-1")).Return(fmt.Errorf("test error")).Once()
 
 			expectedSipherKey := "1234-5678"
 
-			a := NewActions(map[string]ActionFactoryFunc{
-				"test-action": func(cipher string) (secret.Provider, func()) {
+			a := NewMethods(map[string]MethodFactoryFunc{
+				"test-method": func(cipher string) (secret.Provider, func()) {
 					require.EqualValues(t, expectedSipherKey, cipher)
-					return mockProvider, func() {}
+					return mockProvider, nil
 				},
 			})
 
 			s := httptest.NewServer(http.HandlerFunc(a.SetByKey))
 			defer s.Close()
 
-			body := bytes.NewBufferString(`{"getter":"test-getter-1","action":"test-action","value":"test-value-1"}`)
+			body := bytes.NewBufferString(`{"getter":"test-getter-1","method":"test-method","value":"test-value-1"}`)
 			req := httptest.NewRequest(http.MethodPost, s.URL, body)
 			req.Header.Set(ParamCipherKey, expectedSipherKey)
 			req.RequestURI = ""
@@ -92,11 +105,168 @@ func TestHTTPAction(t *testing.T) {
 			require.NoError(t, err)
 			require.EqualValues(t, http.StatusInternalServerError, resp.StatusCode)
 
-			mockProvider.AssertExpectations(t)
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.EqualValues(t, `{"error":"cannot set data: test error"}`, respBody)
+		})
+
+		t.Run("error when cannot decode request body", func(t *testing.T) {
+			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
+
+			a := NewMethods(map[string]MethodFactoryFunc{})
+
+			s := httptest.NewServer(http.HandlerFunc(a.SetByKey))
+			defer s.Close()
+
+			body := bytes.NewBufferString(`abcd`)
+			req := httptest.NewRequest(http.MethodPost, s.URL, body)
+			req.RequestURI = ""
+
+			resp, err := s.Client().Do(req)
+			require.NoError(t, err)
+			require.EqualValues(t, http.StatusBadRequest, resp.StatusCode)
+
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.Contains(t, string(respBody), `{"error":"cannot decode body:`)
 		})
 	})
 
 	t.Run("get by key", func(t *testing.T) {
+		t.Run("success", func(t *testing.T) {
+			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
+			mockProvider.On("GetData", []byte("test-getter-1")).Return([]byte("test-value-1"), nil).Once()
 
+			expectedSipherKey := "1234-5678"
+
+			tearDownFnCounter := int64(1)
+			a := NewMethods(map[string]MethodFactoryFunc{
+				"test-method": func(cipher string) (secret.Provider, func()) {
+					require.EqualValues(t, expectedSipherKey, cipher)
+					return mockProvider, func() {
+						atomic.AddInt64(&tearDownFnCounter, -1)
+					}
+				},
+			})
+
+			s := httptest.NewServer(http.HandlerFunc(a.GetByKey))
+			defer s.Close()
+
+			req := httptest.NewRequest(http.MethodGet, s.URL, nil)
+			req.RequestURI = ""
+			req.Header.Set(ParamCipherKey, expectedSipherKey)
+			query := req.URL.Query()
+			query.Set(ParamGetterKey, "test-getter-1")
+			query.Set(ParamMethodKey, "test-method")
+			req.URL.RawQuery = query.Encode()
+
+			resp, err := s.Client().Do(req)
+			require.NoError(t, err)
+			require.EqualValues(t, http.StatusOK, resp.StatusCode)
+
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.EqualValues(t, `{"value":"test-value-1"}`+jsonTerminator, string(respBody))
+
+			require.Eventually(t, func() bool {
+				return atomic.LoadInt64(&tearDownFnCounter) == 0
+			}, 300*time.Millisecond, 100*time.Millisecond)
+		})
+
+		t.Run("error when no getter provided", func(t *testing.T) {
+			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
+
+			a := NewMethods(map[string]MethodFactoryFunc{})
+
+			s := httptest.NewServer(http.HandlerFunc(a.GetByKey))
+			defer s.Close()
+
+			req := httptest.NewRequest(http.MethodGet, s.URL, nil)
+			req.RequestURI = ""
+			query := req.URL.Query()
+			query.Set(ParamGetterKey, "")
+
+			resp, err := s.Client().Do(req)
+			require.NoError(t, err)
+			require.EqualValues(t, http.StatusBadRequest, resp.StatusCode)
+
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.EqualValues(t, `{"error":"cannot find getter key: empty"}`, respBody)
+		})
+
+		t.Run("error when no method type exists", func(t *testing.T) {
+			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
+
+			tearDownFnCounter := int64(1)
+			a := NewMethods(map[string]MethodFactoryFunc{
+				"test-method": func(cipher string) (secret.Provider, func()) {
+					t.Errorf("test method should not be invoked")
+					return mockProvider, func() {
+						atomic.AddInt64(&tearDownFnCounter, -1)
+					}
+				},
+			})
+
+			s := httptest.NewServer(http.HandlerFunc(a.GetByKey))
+			defer s.Close()
+
+			req := httptest.NewRequest(http.MethodGet, s.URL, nil)
+			req.RequestURI = ""
+			query := req.URL.Query()
+			query.Set(ParamGetterKey, "test-getter-1")
+			query.Set(ParamMethodKey, "test-method-1")
+			req.URL.RawQuery = query.Encode()
+
+			resp, err := s.Client().Do(req)
+			require.NoError(t, err)
+			require.EqualValues(t, http.StatusBadRequest, resp.StatusCode)
+
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.EqualValues(t, `{"error":"cannot find provided method type test-method-1"}`, respBody)
+
+			require.Eventually(t, func() bool {
+				return atomic.LoadInt64(&tearDownFnCounter) == 1 // NOTE: do not expect to be changed
+			}, 300*time.Millisecond, 100*time.Millisecond)
+		})
+
+		t.Run("error when GetData returned error", func(t *testing.T) {
+			mockProvider := new(MockProvider)
+			defer mockProvider.AssertExpectations(t)
+			mockProvider.On("GetData", []byte("test-getter-1")).Return(nil, fmt.Errorf("test error")).Once()
+
+			expectedSipherKey := "1234-5678"
+
+			a := NewMethods(map[string]MethodFactoryFunc{
+				"test-method": func(cipher string) (secret.Provider, func()) {
+					require.EqualValues(t, expectedSipherKey, cipher)
+					return mockProvider, nil
+				},
+			})
+
+			s := httptest.NewServer(http.HandlerFunc(a.GetByKey))
+			defer s.Close()
+
+			req := httptest.NewRequest(http.MethodGet, s.URL, nil)
+			req.RequestURI = ""
+			req.Header.Set(ParamCipherKey, expectedSipherKey)
+			query := req.URL.Query()
+			query.Set(ParamGetterKey, "test-getter-1")
+			query.Set(ParamMethodKey, "test-method")
+			req.URL.RawQuery = query.Encode()
+
+			resp, err := s.Client().Do(req)
+			require.NoError(t, err)
+			require.EqualValues(t, http.StatusInternalServerError, resp.StatusCode)
+
+			respBody, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.Contains(t, string(respBody), `{"error":"cannot get data by key:`)
+		})
 	})
 }

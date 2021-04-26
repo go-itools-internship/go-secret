fda0c5e80a0af5c3e05924ab2e2d1d1d1f03fc01
ViktorRyneiski <59069235+ViktorRyneiski@users.noreply.github.com>
2021-04-26 14:51:33 +0300

    Feature/design file storage for keeping records in file system (#9)
    * reflections and sketches
    
    * reflections and sketches
    
    * Finished interfaces and add descriptions.
    
    * Delete file
    
    * Designing a file storage to store records in the file system
    
    * Add failVault, implemented dataSaver, add tests
    
    * Fix failVault constructor
    
    * fix comment
    
    * fix
    
    * Add Close() method to fileVault and fix tests
    
    * Fix linters wishes
    
    * Fix logic and tests
    
    * Fix logic and tests
    
    * Add go.mod
    
    * Add go.sum
    
    * Add modules.txt
    
    * Fix tests and go.mod
    
    * delete go.sum
    
    * delete go.sum
    
    * Add testify
    
    * Add go.sum
    
    * Add vendor
    
    * Add imports
    
    * Add imports
    
    * Fix description storage.go
    
    * Update token
    
    * Update imports
    
    * Fix: add testStorageMap to test, renamed Storage -> storage in storage.go, add check error in constructor fileVault
    
    * fix: use simple path for error handling
    
    * add scope for errors
    
    * refactor tests
    
    * style: fix lint
    
    Co-authored-by: Alex Rudyak <aliaksandr.rudziak@instinctools.com>

---
+++ pkg/io/storage/storage.go
@@ -0,0 +1,89 @@
+/*
+Package storage provides functions for storing and retrieving data.
+*/
+package storage
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type fileVault struct {
+	storage map[string][]byte
+	path    string
+}
+
+func NewFileVault(path string) (*fileVault, error) {
+	storage := make(map[string][]byte)
+
+	f, err := os.Open(filepath.Clean(path))
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil, fmt.Errorf("filevault: unable to open file: %w", err)
+		}
+		if f, err = os.Create(path); err != nil {
+			return nil, fmt.Errorf("filevault: unable to create file: %w", err)
+		}
+		if _, err := fmt.Fprintf(f, `{}`); err != nil {
+			return nil, fmt.Errorf("filevault: unable to init file: %w", err)
+		}
+	}
+	defer func() {
+		if cerr := f.Close(); err == nil {
+			err = fmt.Errorf("filevault: %w", cerr)
+		}
+	}()
+
+	if err := json.NewDecoder(f).Decode(&storage); err != nil {
+		if !errors.Is(err, io.EOF) {
+			return nil, fmt.Errorf("filevault: unable to decode data: %w", err)
+		}
+	}
+
+	return &fileVault{storage: storage, path: path}, nil
+}
+
+func (f *fileVault) SaveData(key, encodedValue []byte) error {
+	file, err := os.OpenFile(f.path, os.O_WRONLY, 0600)
+	if err != nil {
+		return fmt.Errorf("filevault: unable to open file while saving: %w", err)
+	}
+	defer func() {
+		if cerr := file.Close(); err == nil {
+			err = fmt.Errorf("filevault: %w", cerr)
+		}
+	}()
+
+	f.storage[string(key)] = encodedValue
+	if err = json.NewEncoder(file).Encode(f.storage); err != nil {
+		return fmt.Errorf("filevault: unable to encode data while saving: %w", err)
+	}
+	return nil
+}
+
+func (f *fileVault) ReadData(key []byte) ([]byte, error) {
+	file, err := os.OpenFile(f.path, os.O_RDONLY, 0600)
+	if err != nil {
+		return nil, fmt.Errorf("filevault: unable to open file while reading: %w", err)
+	}
+	defer func() {
+		if cerr := file.Close(); err == nil {
+			err = fmt.Errorf("filevault: %w", cerr)
+		}
+	}()
+
+	if err := json.NewDecoder(file).Decode(&f.storage); err != nil {
+		return nil, fmt.Errorf("filevault: unable to decode while reading: %w", err)
+	}
+
+	data, ok := f.storage[string(key)]
+	if !ok {
+		return nil, fmt.Errorf("filevault: cannot read data: not found")
+	}
+
+	return data, nil
+}
---
+++ pkg/io/storage/storage_test.go
@@ -0,0 +1,60 @@
+package storage
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testFilename = "testfile.json"
+
+func TestFileVault(t *testing.T) {
+	fileVault, err := NewFileVault(testFilename)
+	require.NoError(t, err)
+
+	defer func() {
+		err = os.Remove(testFilename)
+		if err != nil {
+			t.Logf("file not removed")
+		}
+	}()
+
+	t.Run("SaveData", func(t *testing.T) {
+		want := []byte("Hey")
+
+		require.NoError(t, fileVault.SaveData([]byte("f1"), want))
+
+		f, err := os.OpenFile(fileVault.path, os.O_RDONLY, 0600)
+		require.NoError(t, err)
+		defer func() {
+			require.NoError(t, f.Close())
+		}()
+
+		testStorage := make(map[string][]byte)
+		require.NoError(t, json.NewDecoder(f).Decode(&testStorage))
+		got := testStorage["f1"]
+
+		require.EqualValues(t, want, got)
+	})
+
+	t.Run("ReadData", func(t *testing.T) {
+		want := []byte("World")
+
+		f, err := os.OpenFile(fileVault.path, os.O_WRONLY, 0600)
+		require.NoError(t, err)
+		defer func() {
+			require.NoError(t, f.Close())
+		}()
+
+		testStorage := make(map[string][]byte)
+		testStorage["f2"] = want
+
+		require.NoError(t, json.NewEncoder(f).Encode(testStorage))
+
+		got, err := fileVault.ReadData([]byte("f2"))
+		require.NoError(t, err)
+		require.EqualValues(t, want, got)
+	})
+}

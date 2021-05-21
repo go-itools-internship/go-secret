83c08687db01051f19932f2e317c13c7b535a002
BirukouAliaksei <birukou.aliaksei@gmail.com>
2021-05-21 12:13:40 +0300

    fix documentation

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -14,18 +14,43 @@ import (
 )
 
 type client struct {
-	client http.Client // TODO client options
-	url    string      // address where client will be work with server
+	options Options
+	url     string // address where client will be work with server
 }
 
-// New function creates client with timeout
-func New(url string) *client {
-	client1 := http.Client{Timeout: 20 * time.Second}
-	newClient := &client{client: client1, url: url}
+// Options client
+type Options struct {
+	c http.Client
+}
+
+var defaultClient = Options{
+	c: http.Client{Timeout: 20 * time.Second},
+}
+
+type OptionsOfClient func(o *Options)
+
+// Client provide any http client
+func Client(c http.Client) OptionsOfClient {
+	return func(options *Options) {
+		options.c = c
+	}
+}
+
+// New function initializes a structure that provides client accessing functions
+// Accepts url where client will be work with server and client options
+func New(url string, opts ...OptionsOfClient) *client {
+	options := defaultClient
+	for _, opt := range opts {
+		opt(&options)
+	}
+	newClient := &client{options: options, url: url}
 	return newClient
 }
 
 // GetByKey get data from server by key method and cipher key
+// key for pair key-value
+// cipher key for data encryption and decryption
+// method to choose different providers
 func (c *client) GetByKey(ctx context.Context, key, method, cipherKey string) (string, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.url, nil)
 	if err != nil {
@@ -38,13 +63,13 @@ func (c *client) GetByKey(ctx context.Context, key, method, cipherKey string) (s
 	query.Set(api.ParamMethodKey, method)
 	req.URL.RawQuery = query.Encode()
 
-	resp, err := c.client.Do(req)
+	resp, err := c.options.c.Do(req)
 	if err != nil {
 		return "", fmt.Errorf("secret client: can't do response %w", err)
 	}
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
-			fmt.Println("cannot close request body: ", err.Error())
+			fmt.Println("secret client: cannot close request body: ", err.Error())
 		}
 	}()
 
@@ -53,7 +78,7 @@ func (c *client) GetByKey(ctx context.Context, key, method, cipherKey string) (s
 	}
 
 	if err := json.NewDecoder(resp.Body).Decode(&responseBody); err != nil {
-		return "", fmt.Errorf("cannot decode body: %w", err)
+		return "", fmt.Errorf("secret client: cannot decode body: %w", err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
@@ -62,7 +87,10 @@ func (c *client) GetByKey(ctx context.Context, key, method, cipherKey string) (s
 	return responseBody.Value, nil
 }
 
-// SetByKey set data to server by  getterKey, value, method, cipherKey
+// SetByKey set data to server by getterKey, value, method, cipherKey
+// getterKey for pair key-value
+// cipher key to set data encryption
+// method to choose different providers
 func (c *client) SetByKey(ctx context.Context, getterKey, value, method, cipherKey string) error {
 	postBody, err := json.Marshal(map[string]string{
 		"getter": getterKey,
@@ -79,13 +107,13 @@ func (c *client) SetByKey(ctx context.Context, getterKey, value, method, cipherK
 	}
 	req.Header.Set(api.ParamCipherKey, cipherKey)
 
-	resp, err := c.client.Do(req)
+	resp, err := c.options.c.Do(req)
 	if err != nil {
 		return fmt.Errorf("secret client: can't do request %w", err)
 	}
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
-			fmt.Println("cannot close request body: ", err.Error())
+			fmt.Println("secret client: cannot close request body: ", err.Error())
 		}
 	}()
 

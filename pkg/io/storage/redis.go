883930597c748a69a30525f7a3edb1fdb41d8dd9
BirukouAliaksei <birukou.aliaksei@gmail.com>
2021-06-02 17:00:22 +0300

    add redis to docker-compose, add redis storage and tests

--- pkg/io/storage/redis.go
+++ pkg/io/storage/redis.go
@@ -1,9 +1,10 @@
 package storage
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
-	"net/http"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -13,26 +14,50 @@ type redisVault struct {
 	ctx         context.Context
 }
 
-func New(ctx context.Context, redisClient http.Client) (*redisVault, error) {
-	rdb := redis.NewClient(&redis.Options{Addr: "", Password: "", DB: 0})
+// New create new redis client with context
+func New(ctx context.Context) *redisVault {
+	rdb := redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
 	rv := &redisVault{
 		rdb, ctx,
 	}
-	return rv, nil
+	return rv
 }
 
+// SaveData put data in redis storage by key and encoded value
+// 	key to set in redis storage (can't be nil)
+// 	encoded value to storage
 func (r *redisVault) SaveData(key, encodedValue []byte) error {
+	if bytes.Equal(key, []byte("")) {
+		return fmt.Errorf("storage: %w", errors.New("Key can't be nil "))
+	}
+	if bytes.Equal(encodedValue, []byte("")) {
+		fmt.Println("Key was deleted")
+		res, err := r.redisClient.Del(r.ctx, string(key)).Result()
+		fmt.Println(res)
+		if err != nil {
+			return fmt.Errorf("storage: %w", err)
+		}
+		return nil
+	}
 	err := r.redisClient.Set(r.ctx, string(key), encodedValue, 0).Err()
 	if err != nil {
-		fmt.Println(err)
+		return fmt.Errorf("storage: redis client can't set data %w", err)
 	}
 	return nil
 }
 
+// ReadData get data from redis storage by key
+// 	key to get value for pair key-value from redis storage (can't be nil)
 func (r *redisVault) ReadData(key []byte) ([]byte, error) {
+	if bytes.Equal(key, []byte("")) {
+		return nil, fmt.Errorf("storage: %w", errors.New("Key can't be nil "))
+	}
 	val, err := r.redisClient.Get(r.ctx, string(key)).Result()
 	if err != nil {
-		return nil, err
+		if errors.Is(err, redis.Nil) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("storage: redis client can't get data %w", err)
 	}
 	return []byte(val), nil
 }
--- pkg/io/storage/redis_test.go
+++ pkg/io/storage/redis_test.go
@@ -1,1 +1,66 @@
 package storage
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRedisVault_SaveData(t *testing.T) {
+	key := "key"
+	encodedValue := "value"
+	t.Run("success", func(t *testing.T) {
+		ctx := context.Background()
+		s := New(ctx)
+		err := s.SaveData([]byte(key), []byte(encodedValue))
+		require.NoError(t, err)
+	})
+	t.Run("error if key equals nil", func(t *testing.T) {
+		key := ""
+		ctx := context.Background()
+		s := New(ctx)
+		err := s.SaveData([]byte(key), []byte(encodedValue))
+		require.Error(t, err)
+		require.EqualValues(t, "storage: Key can't be nil ", err.Error())
+	})
+	t.Run("wrong key error if key has been deleted", func(t *testing.T) {
+		nilEncodedValue := ""
+		ctx := context.Background()
+		s := New(ctx)
+		err := s.SaveData([]byte(key), []byte(key))
+		require.NoError(t, err)
+		err = s.SaveData([]byte(key), []byte(nilEncodedValue))
+		require.NoError(t, err)
+
+		val, err := s.ReadData([]byte(key))
+		require.NoError(t, err)
+		require.NotEqualValues(t, "", val)
+	})
+}
+
+func TestRedisVault_ReadData(t *testing.T) {
+	key := "key"
+	encodedValue := "value"
+	t.Run("success", func(t *testing.T) {
+		ctx := context.Background()
+		s := New(ctx)
+
+		err := s.SaveData([]byte(key), []byte(encodedValue))
+		require.NoError(t, err)
+
+		val, err := s.ReadData([]byte(key))
+		require.NoError(t, err)
+		fmt.Println(string(val))
+		require.EqualValues(t, "value", val)
+	})
+	t.Run("error if wrong key", func(t *testing.T) {
+		wrongKey := "wrongKey"
+		ctx := context.Background()
+		s := New(ctx)
+		val, err := s.ReadData([]byte(wrongKey))
+		require.NoError(t, err)
+		require.NotEqualValues(t, "value", val)
+	})
+}

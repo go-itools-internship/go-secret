268c03b4342b0e1d276d5a338c68343510141620
BirukouAliaksei <birukou.aliaksei@gmail.com>
2021-06-14 14:10:14 +0300

    fix context time

--- cmd/secret/cmd/root_get_test.go
+++ cmd/secret/cmd/root_get_test.go
@@ -54,7 +54,7 @@ func TestRoot_Get(t *testing.T) {
 		require.EqualValues(t, value, got)
 	})
 	t.Run("success after get redis command", func(t *testing.T) {
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
 		r := New()
@@ -75,7 +75,7 @@ func TestRoot_Get(t *testing.T) {
 	})
 	t.Run("success after get postgres command", func(t *testing.T) {
 		postgresURL := "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
 		r := New()

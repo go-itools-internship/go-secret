14f8267c27d330a08f7bbc4a5a28e88c649be9a4
BirukouAliaksei <birukou.aliaksei@gmail.com>
2021-06-15 12:15:45 +0300

    try to find error

--- cmd/secret/cmd/root_test.go
+++ cmd/secret/cmd/root_test.go
@@ -50,39 +50,6 @@ func TestRoot_Server(t *testing.T) {
 			require.EqualValues(t, http.StatusNoContent, resp.StatusCode)
 			require.NoError(t, resp.Body.Close())
 		})
-		t.Run("expect postgres set method success", func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-			defer cancel()
-			defer migrateDown()
-			port, err := GetFreePort()
-			require.NoError(t, err)
-			r := New()
-			r.cmd.SetArgs([]string{"server", "--port", strconv.Itoa(port), "--postgres-url", postgresURL, "--migration", "file://../../../"})
-			go func() {
-				err := r.Execute(ctx)
-				if err != nil {
-					fmt.Println(err)
-				}
-			}()
-			time.Sleep(2 * time.Second)
-			defer func() {
-				require.NoError(t, os.Remove("file.txt"))
-			}()
-
-			client := http.Client{Timeout: time.Second}
-			body := bytes.NewBufferString(`{"getter":"key-value","method":"remote","value":"test-value-1"}`)
-			req := httptest.NewRequest(http.MethodPost, "http://localhost:"+strconv.Itoa(port), body)
-			req.Header.Set(api.ParamCipherKey, expectedSipherKey)
-			req.RequestURI = ""
-
-			resp, err := client.Do(req)
-			require.NoError(t, err)
-			data, err := ioutil.ReadAll(resp.Body)
-			require.NoError(t, err)
-			fmt.Println(string(data), resp.Header)
-			require.EqualValues(t, http.StatusNoContent, resp.StatusCode)
-			require.NoError(t, resp.Body.Close())
-		})
 		t.Run("expect redis set method success", func(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 			defer cancel()
@@ -128,6 +95,39 @@ func TestRoot_Server(t *testing.T) {
 			resp, err := client.Do(req)
 			require.NoError(t, err)
 			require.EqualValues(t, http.StatusNotFound, resp.StatusCode)
+			require.NoError(t, resp.Body.Close())
+		})
+		t.Run("expect postgres set method success", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			defer cancel()
+			defer migrateDown()
+			port, err := GetFreePort()
+			require.NoError(t, err)
+			r := New()
+			r.cmd.SetArgs([]string{"server", "--port", strconv.Itoa(port), "--postgres-url", postgresURL, "--migration", "file://../../../"})
+			go func() {
+				err := r.Execute(ctx)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}()
+			time.Sleep(2 * time.Second)
+			defer func() {
+				require.NoError(t, os.Remove("file.txt"))
+			}()
+
+			client := http.Client{Timeout: time.Second}
+			body := bytes.NewBufferString(`{"getter":"key-value","method":"remote","value":"test-value"}`)
+			req := httptest.NewRequest(http.MethodPost, "http://localhost:"+strconv.Itoa(port), body)
+			req.Header.Set(api.ParamCipherKey, expectedSipherKey)
+			req.RequestURI = ""
+
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			data, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err)
+			fmt.Println(string(data), resp.Header)
+			require.EqualValues(t, http.StatusNoContent, resp.StatusCode)
 			require.NoError(t, resp.Body.Close())
 		})
 	})

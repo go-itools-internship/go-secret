784da0cb4cee35e234a53fb82c21216555d713e0
BirukouAliaksei <birukou.aliaksei@gmail.com>
2021-06-15 11:49:04 +0300

    test flaky test

--- cmd/secret/cmd/root_test.go
+++ cmd/secret/cmd/root_test.go
@@ -77,6 +77,9 @@ func TestRoot_Server(t *testing.T) {
 
 			resp, err := client.Do(req)
 			require.NoError(t, err)
+			data, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err)
+			fmt.Println(string(data), resp.Header)
 			require.EqualValues(t, http.StatusNoContent, resp.StatusCode)
 			require.NoError(t, resp.Body.Close())
 		})
